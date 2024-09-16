dfa562114b5228bce00ce684a723531772f4c239
wasupon <wasupon.mail@gmail.com>
2024-09-16 11:14:32 +0900

    feat: contact form overlay

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,54 @@
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-  const weatherData = await weatherPromise.json()
+  const weatherPromise = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  );
+  const weatherData = await weatherPromise.json();
 
-  const ourTemperature = weatherData.properties.periods[0].temperature
-  document.querySelector("#temperature-output").textContent = ourTemperature
-
+  const ourTemperature = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
-start()
+start();
 
 // pet filter button code
-const allButtons = document.querySelectorAll(".pet-filter button")
+const allButtons = document.querySelectorAll(".pet-filter button");
 
-allButtons.forEach(el => {
-  el.addEventListener("click", handleButtonClick)
-})
+allButtons.forEach((el) => {
+  el.addEventListener("click", handleButtonClick);
+});
 
 function handleButtonClick(e) {
   // remove active class from any and all buttons
-  allButtons.forEach(el => el.classList.remove("active"))
+  allButtons.forEach((el) => el.classList.remove("active"));
 
   // add active class to the specific button that just got clicked
-  e.target.classList.add("active")
+  e.target.classList.add("active");
 
   // actually filter the pets down below
-  const currentFilter = e.target.dataset.filter
-  document.querySelectorAll(".pet-card").forEach(el => {
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
     if (currentFilter == el.dataset.species || currentFilter == "all") {
-      el.style.display = "grid"
+      el.style.display = "grid";
     } else {
-      el.style.display = "none"
+      el.style.display = "none";
     }
-  })
+  });
+}
+
+document.querySelector(".form-overlay").style.display = "";
+
+function openOverlay(el) {
+  document
+    .querySelector(".form-overlay")
+    .classList.add("form-overlay--is-visible");
+}
+
+document
+  .querySelector(".close-form-overlay")
+  .addEventListener("click", closeOverlay);
+
+function closeOverlay() {
+  document
+    .querySelector(".form-overlay")
+    .classList.remove("form-overlay--is-visible");
 }
